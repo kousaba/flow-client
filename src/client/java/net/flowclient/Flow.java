b515827eee8d13704074ab10392e14918b4aeb3a
kousaba <kousaba1@proton.me>
2025-12-28 14:09:12 +0900

    ModuleManagerを作成

--- src/client/java/net/flowclient/Flow.java
+++ src/client/java/net/flowclient/Flow.java
@@ -1,11 +1,19 @@
 package net.flowclient;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.flowclient.module.ModuleManager;
+
+import java.io.File;
 
 public class Flow implements ClientModInitializer {
     public static final Flow INSTANCE = new Flow();
+    public ModuleManager moduleManager;
     @Override
     public void onInitializeClient(){
         System.out.println("Flow Client Initializing...");
+        File dir = new File("flow_config");
+        if(!dir.exists()) dir.mkdir();
+        moduleManager = new ModuleManager(dir);
+        moduleManager.loadConfig();
     }
 }
---
+++ src/client/java/net/flowclient/module/ModuleManager.java
@@ -0,0 +1,72 @@
+package net.flowclient.module;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.DrawContext;
+
+import java.io.*;
+import java.util.Collection;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class ModuleManager {
+    private final Map<Class<? extends Module>, Module> modules = new LinkedHashMap<>();
+    private final File configFile;
+    private final Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+    public ModuleManager(File dataDir){
+        this.configFile = new File(dataDir, "modules.json");
+
+    }
+
+    private void addModule(Module module){
+        modules.put(module.getClass(), module);
+    }
+
+    // モジュール取得
+    @SuppressWarnings("unchecked")
+    public <T extends Module> T getModule(Class<T> clazz){
+        return (T) modules.get(clazz);
+    }
+
+    // すべてのモジュール取得
+    public Collection<Module> getModules(){
+        return modules.values();
+    }
+
+    public void saveConfig(){
+        JsonObject json = new JsonObject();
+        for(Module module : modules.values()){
+            json.add(module.name, module.saveAll());
+        }
+        try(Writer writer = new FileWriter(configFile)){
+            gson.toJson(json, writer);
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public void loadConfig(){
+        if(!configFile.exists()) return;
+        try(Reader reader = new FileReader(configFile)){
+            JsonObject json = gson.fromJson(reader, JsonObject.class);
+            for(Module module : modules.values()){
+                if(json.has(module.name)){
+                    module.loadAll(json.getAsJsonObject(module.name));
+                }
+            }
+        } catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public void render(DrawContext context){
+        for(Module module : modules.values()){
+            if(module instanceof HudModule hud){
+                hud.render(context);
+            }
+        }
+    }
+}
